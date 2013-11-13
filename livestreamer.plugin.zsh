3cd3f05ab70155a62bc4e1185c027ccac2861f61
Jason R. McNeil <jason@jasonrm.net>
2013-11-12 19:56:33 -0700

    Set screenshot folder to home directory. Don't resuse channels settings

--- livestreamer.plugin.zsh
+++ livestreamer.plugin.zsh
@@ -5,10 +5,12 @@ twitch(){
     local o_resize_resolution=$4
     local o_pan=$5
 
+    local screenshot_folder="--screenshot-format=png --screenshot-template=\"${HOME}/${o_stream} - %tY-%tm-%td at %tH.%tM.%tS\""
+
     local scale=""
     if [ "${o_original_resolution}" != "${o_resize_resolution}p" ]; then
         mplayer_scale="-sws 9 -vf scale=-3:${o_resize_resolution}"
-        mpv_scale="-${mplayer_scale}"
+        mpv_scale=""
     else
         scale=""
     fi
@@ -20,13 +22,16 @@ twitch(){
     elif [ "${o_pan}" = "right" ]; then
         mplayer_channels="-af channels=2:2:0:1:1:1"
         mpv_channels="-${mplayer_channels}"
+    else
+        mplayer_channels=""
+        mpv_channels=""
     fi
 
     local player=""
     if [ "${o_player}" = "mplayer" ]; then
         player="/usr/local/bin/mplayer -framedrop -af comp -cache 2048 -cache-min 50 -title ${o_stream} ${mplayer_scale} ${mplayer_channels}"
     elif [ "${o_player}" = "mpv" ]; then
-        player="/usr/local/bin/mpv --quiet --vo=opengl-hq --framedrop=yes --hwdec=vda --cache=2048 --cache-min=50 --title=${o_stream} ${mpv_scale} ${mpv_channels}"
+        player="/usr/local/bin/mpv --vo=corevideo --framedrop=yes --hwdec=vda --cache=2048 --cache-min=50 --title=${o_stream} ${mpv_scale} ${mpv_channels} ${screenshot_folder}"
     fi
 
     /usr/local/bin/livestreamer -v --player "$player" "twitch.tv/${o_stream}" $o_original_resolution
