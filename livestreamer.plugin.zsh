e206d4e11507256a72e5afd36b1a3f0700304fd8
Jason R. McNeil <jason@jasonrm.net>
2015-07-26 11:19:23 +0000

    Drop mplayer support and cleanup config

--- livestreamer.plugin.zsh
+++ livestreamer.plugin.zsh
@@ -1,39 +1,24 @@
 twitch(){
-    local o_player=$1
-    local o_stream=$2
-    local o_original_resolution=$3
-    local o_resize_resolution=$4
-    local o_pan=$5
+    local o_stream=$1
+    local o_original_resolution=$2
+    local o_resize_resolution=$3
+    local o_pan=$4
 
     local screenshot_folder="--screenshot-format=png --screenshot-template=\"${HOME}/${o_stream} - %tY-%tm-%td at %tH.%tM.%tS\""
 
     local scale=""
     if [ "${o_original_resolution}" != "${o_resize_resolution}p" ]; then
-        mplayer_scale="-sws 9 -vf scale=-3:${o_resize_resolution}"
-        mpv_scale="-${mplayer_scale}"
-    else
-        mplayer_scale=""
-        mpv_scale=""
+        scale="--sws 9 -vf scale=-3:${o_resize_resolution}"
     fi
 
     local channels=""
     if [ "${o_pan}" = "left" ]; then
-        mplayer_channels="-af channels=2:2:0:0:1:0"
-        mpv_channels="-${mplayer_channels}"
+        channels="--af channels=2:2:0:0:1:0"
     elif [ "${o_pan}" = "right" ]; then
-        mplayer_channels="-af channels=2:2:0:1:1:1"
-        mpv_channels="-${mplayer_channels}"
-    else
-        mplayer_channels=""
-        mpv_channels=""
+        channels="--af channels=2:2:0:1:1:1"
     fi
 
-    local player=""
-    if [ "${o_player}" = "mplayer" ]; then
-        player="/usr/local/bin/mplayer -framedrop -af comp -cache 2048 -cache-min 50 -title ${o_stream} ${mplayer_scale} ${mplayer_channels}"
-    elif [ "${o_player}" = "mpv" ]; then
-        player="/usr/local/bin/mpv --cache=2048 --cache-min=50 --title=${o_stream} ${mpv_scale} ${mpv_channels} ${screenshot_folder}"
-    fi
+    player="mpv --title=${o_stream} ${scale} ${channels} ${screenshot_folder}"
 
-    /usr/local/bin/livestreamer -v --player "$player" "twitch.tv/${o_stream}" $o_original_resolution
+    livestreamer -v --player "$player" "twitch.tv/${o_stream}" $o_original_resolution
 }
